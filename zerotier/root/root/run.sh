41534b151b259848b60c420815029f6819a2f266
RouteIX <77181250+RouteIX@users.noreply.github.com>
2021-01-27 18:33:07 +1030

    Update run.sh

--- zerotier/root/root/run.sh
+++ zerotier/root/root/run.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin
+
+if [ ! -e /dev/net/tun ]; then
+	echo 'FATAL: cannot start ZeroTier One in container: /dev/net/tun not present.'
+	exit 1
+fi
 
 #zerotier-one
 supervisord -c /etc/supervisor/supervisord.conf
 
-[ ! -z $NETWORK_ID ] && { sleep 5; zerotier-cli join $NETWORK_ID;zerotier-cli set $NETWORK_ID allowGlobal=true; || exit 1; }
+if [ ! -e /var/lib/zerotier-one/networks.d/$NETWORK_ID.conf ]; then
+	sleep 3;
+  zerotier-cli join $NETWORK_ID;
+  zerotier-cli set $NETWORK_ID allowGlobal=true;
+fi
 
-[ ! -z $NETWORK_REGIONAL ] && { sleep 5; zerotier-cli join $NETWORK_REGIONAL;zerotier-cli set $NETWORK_REGIONAL allowGlobal=true; || exit 1; }
+if [ ! -e /var/lib/zerotier-one/networks.d/$NETWORK_REGIONAL.conf ]; then
+	sleep 3; 
+  zerotier-cli join $NETWORK_REGIONAL;
+  zerotier-cli set $NETWORK_REGIONAL allowGlobal=true;
+fi
 
 # something that keep the container running
 tail -f /dev/null
